2af28c2995b2631c882d7bd4da8740f4462ab441
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-06-09 01:08:44 +0400

    Extract methods for parsing numbers, strings, dictionaries and lists

--- src/main.rs
+++ src/main.rs
@@ -14,76 +14,96 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
 }
 
 fn decode_bencoded_at_position(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
-    let mut current_position: usize = position;
-    let mut next_symbol = input[current_position];
+    let current_position: usize = position;
+    let next_symbol = input[current_position];
     if next_symbol.is_digit(10) {
-        let mut number_chars: Vec<char> = Vec::new();
-        while next_symbol != ':' {
-            number_chars.push(next_symbol);
-            current_position += 1;
-            next_symbol = input[current_position];
-        }
-        current_position += 1;
-        let string_value_length_input: String = number_chars.into_iter().collect();
-        let string_value_length = string_value_length_input.parse::<usize>().unwrap();
-        let string_value: String = input[current_position..current_position + string_value_length].into_iter().collect();
-
-        (Some(json!(string_value)), current_position + string_value_length)
+        decode_string(input, position)
     } else if next_symbol == 'i' {
-        current_position += 1;
-        next_symbol = input[current_position];
-        let mut number_chars: Vec<char> = Vec::new();
-        while next_symbol != 'e' {
-            number_chars.push(next_symbol);
-            current_position += 1;
-            next_symbol = input[current_position];
-        }
-        let number_input: String = number_chars.into_iter().collect();
-        let number: i64 = number_input.parse::<i64>().unwrap();
-        (Some(json!(number)), current_position + 1) // skip the 'e' symbol
+        decode_number(input, position)
     } else if next_symbol == 'd' {
-        let mut current_position: usize = position + 1;
-        let mut object: HashMap<String, serde_json::Value> = HashMap::new();
-
-        while input[current_position] != 'e' && current_position < input.len() {
-            let (maybe_key, new_position) = decode_bencoded_at_position(input, current_position);
-            current_position = new_position;
-            if input[current_position] != 'e' {
-                if let Some(key) = maybe_key {
-                    let (maybe_value, new_position) = decode_bencoded_at_position(input, current_position);
-                    current_position = new_position;
-                    if let Some(value) = maybe_value {
-                        match key {
-                            serde_json::Value::Number(key_number) => object.insert(key_number.to_string(), value),
-                            serde_json::Value::String(key_string) => object.insert(key_string, value),
-                            _ => None,
-                        };
-                    }
-                }
-            }
-        }
-        if input[current_position] != 'e' {
-            panic!("Malformed input '{:?}' around position {:?}", input, current_position);
-        }
-        (Some(json!(object)), current_position + 1) // skip the 'e' symbol
+        decode_dictionary(input, position)
     } else if next_symbol == 'l' {
-        let mut array_values: Vec<serde_json::Value> = Vec::new();
-        let mut current_position: usize = position + 1;
-        while input[current_position] != 'e' && current_position < input.len() {
-            let (current_decoded_value, updated_position) = decode_bencoded_at_position(input, current_position);
-            current_position = updated_position;
-            if current_decoded_value.is_some() {
-                array_values.push(current_decoded_value.unwrap());
-            }
-        }
-        if input[current_position] != 'e' {
-            panic!("Malformed input '{:?}' around position {:?}", input, current_position);
-        }
-        (Some(serde_json::Value::Array(array_values)), current_position + 1) // skip the 'e' symbol
+        decode_list(input, position)
     } else {
         let unparsed_input: String = input[position..].into_iter().collect();
         panic!("Unhandled encoded value: {}", unparsed_input)
     }
+}
+
+fn decode_list(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+    let mut current_position: usize = position + 1; // skip 'l'
+    let mut array_values: Vec<serde_json::Value> = Vec::new();
+    while input[current_position] != 'e' && current_position < input.len() {
+        let (current_decoded_value, updated_position) = decode_bencoded_at_position(input, current_position);
+        current_position = updated_position;
+        if current_decoded_value.is_some() {
+            array_values.push(current_decoded_value.unwrap());
+        }
+    }
+    if input[current_position] != 'e' {
+        panic!("Malformed input '{:?}' around position {:?}", input, current_position);
+    }
+    (Some(serde_json::Value::Array(array_values)), current_position + 1) // skip 'e'
+}
+
+fn decode_dictionary(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+    let mut current_position: usize = position + 1; // skip 'd'
+    let mut object: HashMap<String, serde_json::Value> = HashMap::new();
+
+    while input[current_position] != 'e' && current_position < input.len() {
+        let (maybe_key, new_position) = decode_bencoded_at_position(input, current_position);
+        current_position = new_position;
+        if input[current_position] != 'e' {
+            if let Some(key) = maybe_key {
+                let (maybe_value, new_position) = decode_bencoded_at_position(input, current_position);
+                current_position = new_position;
+                if let Some(value) = maybe_value {
+                    match key {
+                        serde_json::Value::Number(key_number) => object.insert(key_number.to_string(), value),
+                        serde_json::Value::String(key_string) => object.insert(key_string, value),
+                        _ => None,
+                    };
+                }
+            }
+        }
+    }
+    if input[current_position] != 'e' {
+        panic!("Malformed input '{:?}' around position {:?}", input, current_position);
+    }
+    (Some(json!(object)), current_position + 1) // skip 'e'
+}
+
+fn decode_string(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+    let mut current_position: usize = position;
+    let mut next_symbol = input[current_position];
+
+    let mut number_chars: Vec<char> = Vec::new();
+    while next_symbol != ':' {
+        number_chars.push(next_symbol);
+        current_position += 1;
+        next_symbol = input[current_position];
+    }
+    current_position += 1;
+    let string_value_length_input: String = number_chars.into_iter().collect();
+    let string_value_length = string_value_length_input.parse::<usize>().unwrap();
+    let string_value: String = input[current_position..current_position + string_value_length].into_iter().collect();
+
+    (Some(json!(string_value)), current_position + string_value_length)
+}
+
+fn decode_number(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+    let mut current_position: usize = position + 1;
+    let mut next_symbol = input[current_position];
+
+    let mut number_chars: Vec<char> = Vec::new();
+    while next_symbol != 'e' {
+        number_chars.push(next_symbol);
+        current_position += 1;
+        next_symbol = input[current_position];
+    }
+    let number_input: String = number_chars.into_iter().collect();
+    let number: i64 = number_input.parse::<i64>().unwrap();
+    (Some(json!(number)), current_position + 1) // skip the 'e' symbol
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
