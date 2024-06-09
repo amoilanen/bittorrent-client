e8a133223c03315a790bf41cdb984a9da125e4f4
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-06-09 17:03:17 +0400

    Introduce custom types for representing a parsed Bencoded value, preparatory step before parsing torrent file

--- src/bencoded.rs
+++ src/bencoded.rs
@@ -1,15 +1,54 @@
 use serde_json;
 use serde_json::json;
 use std::collections::HashMap;
+use std::hash::Hash;
+
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum Value {
+    List(Vec<Value>),
+    Number(i64),
+    String(Vec<u8>),
+    Object(Vec<(Value, Value)>)
+}
+
+impl Value {
+    pub(crate) fn as_json(&self) -> serde_json::Value {
+        match self {
+            Value::Number(number) => json!(number),
+            Value::String(string_bytes) => {
+                let string: String = string_bytes.into_iter().map(|x| *x as char).collect();
+                json!(string)
+            },
+            Value::List(values) => {
+                let encoded_values: Vec<serde_json::Value> = values.into_iter().map(|value| value.as_json()).collect();
+                json!(serde_json::Value::Array(encoded_values))
+            },
+            Value::Object(pairs) => {
+                let mut object: HashMap<String, serde_json::Value> = HashMap::new();
+                pairs.iter().map(|(key, value)| {
+                    (key.as_json(), value.as_json())
+                }).for_each(|(key, value)| {
+                    match key {
+                        serde_json::Value::Number(key_number) => object.insert(key_number.to_string(), value),
+                        serde_json::Value::String(key_string) => object.insert(key_string, value),
+                        _ => None,
+                    };
+                });
+                json!(object)
+            }
+        }
+    }
+}
 
 #[allow(dead_code)]
-pub(crate) fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+pub(crate) fn decode_bencoded_from_str(encoded_value: &str) -> Value {
     //TODO: Return a Result, rather than use unwrap
     //TODO: Handle the case when not all of the encoded_value input has been read
     decode_bencoded_at_position(&encoded_value.chars().collect(), 0).0.unwrap()
 }
 
-fn decode_bencoded_at_position(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+fn decode_bencoded_at_position(input: &Vec<char>, position: usize) -> (Option<Value>, usize) {
     let current_position: usize = position;
     let next_symbol = input[current_position];
     if next_symbol.is_digit(10) {
@@ -26,25 +65,25 @@ fn decode_bencoded_at_position(input: &Vec<char>, position: usize) -> (Option<se
     }
 }
 
-fn decode_list(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+fn decode_list(input: &Vec<char>, position: usize) -> (Option<Value>, usize) {
     let mut current_position: usize = position + 1; // skip 'l'
-    let mut array_values: Vec<serde_json::Value> = Vec::new();
+    let mut array_values: Vec<Value> = Vec::new();
     while input[current_position] != 'e' && current_position < input.len() {
         let (current_decoded_value, updated_position) = decode_bencoded_at_position(input, current_position);
         current_position = updated_position;
-        if current_decoded_value.is_some() {
-            array_values.push(current_decoded_value.unwrap());
+        if let Some(decoded_value) = current_decoded_value {
+            array_values.push(decoded_value);
         }
     }
     if input[current_position] != 'e' {
         panic!("Malformed input '{:?}' around position {:?}", input, current_position);
     }
-    (Some(serde_json::Value::Array(array_values)), current_position + 1) // skip 'e'
+    (Some(Value::List(array_values)), current_position + 1) // skip 'e'
 }
 
-fn decode_dictionary(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+fn decode_dictionary(input: &Vec<char>, position: usize) -> (Option<Value>, usize) {
     let mut current_position: usize = position + 1; // skip 'd'
-    let mut object: HashMap<String, serde_json::Value> = HashMap::new();
+    let mut object: HashMap<Value, Value> = HashMap::new();
 
     while input[current_position] != 'e' && current_position < input.len() {
         let (maybe_key, new_position) = decode_bencoded_at_position(input, current_position);
@@ -54,11 +93,7 @@ fn decode_dictionary(input: &Vec<char>, position: usize) -> (Option<serde_json::
                 let (maybe_value, new_position) = decode_bencoded_at_position(input, current_position);
                 current_position = new_position;
                 if let Some(value) = maybe_value {
-                    match key {
-                        serde_json::Value::Number(key_number) => object.insert(key_number.to_string(), value),
-                        serde_json::Value::String(key_string) => object.insert(key_string, value),
-                        _ => None,
-                    };
+                    object.insert(key, value);
                 }
             }
         }
@@ -66,10 +101,10 @@ fn decode_dictionary(input: &Vec<char>, position: usize) -> (Option<serde_json::
     if input[current_position] != 'e' {
         panic!("Malformed input '{:?}' around position {:?}", input, current_position);
     }
-    (Some(json!(object)), current_position + 1) // skip 'e'
+    (Some(Value::Object(object.into_iter().collect())), current_position + 1) // skip 'e'
 }
 
-fn decode_string(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+fn decode_string(input: &Vec<char>, position: usize) -> (Option<Value>, usize) {
     let mut current_position: usize = position;
     let mut next_symbol = input[current_position];
 
@@ -82,12 +117,12 @@ fn decode_string(input: &Vec<char>, position: usize) -> (Option<serde_json::Valu
     current_position += 1;
     let string_value_length_input: String = number_chars.into_iter().collect();
     let string_value_length = string_value_length_input.parse::<usize>().unwrap();
-    let string_value: String = input[current_position..current_position + string_value_length].into_iter().collect();
+    let string_value: Vec<u8> = input[current_position..current_position + string_value_length].into_iter().map(|ch| *ch as u8).collect();
 
-    (Some(json!(string_value)), current_position + string_value_length)
+    (Some(Value::String(string_value)), current_position + string_value_length)
 }
 
-fn decode_number(input: &Vec<char>, position: usize) -> (Option<serde_json::Value>, usize) {
+fn decode_number(input: &Vec<char>, position: usize) -> (Option<Value>, usize) {
     let mut current_position: usize = position + 1;
     let mut next_symbol = input[current_position];
 
@@ -99,7 +134,7 @@ fn decode_number(input: &Vec<char>, position: usize) -> (Option<serde_json::Valu
     }
     let number_input: String = number_chars.into_iter().collect();
     let number: i64 = number_input.parse::<i64>().unwrap();
-    (Some(json!(number)), current_position + 1) // skip the 'e' symbol
+    (Some(Value::Number(number)), current_position + 1) // skip the 'e' symbol
 }
 
 #[cfg(test)]
@@ -109,22 +144,22 @@ mod tests {
 
     #[test]
     fn decode_bencoded_string() {
-        assert_eq!(decode_bencoded_value("4:spam"), json!("spam".to_string()));
+        assert_eq!(decode_bencoded_from_str("4:spam").as_json(), json!("spam".to_string()));
     }
 
     #[test]
     fn decode_bencoded_integers() {
-        assert_eq!(decode_bencoded_value("i52e"), json!(52));
+        assert_eq!(decode_bencoded_from_str("i52e").as_json(), json!(52));
     }
 
     #[test]
     fn decode_bencoded_lists() {
-        assert_eq!(decode_bencoded_value("l5:helloi52ee"), json!(["hello", 52]));
+        assert_eq!(decode_bencoded_from_str("l5:helloi52ee").as_json(), json!(["hello", 52]));
     }
 
     #[test]
     fn decode_bencoded_dictionary() {
-        assert_eq!(decode_bencoded_value("d3:foo3:bar5:helloi52ee"), json!({
+        assert_eq!(decode_bencoded_from_str("d3:foo3:bar5:helloi52ee").as_json(), json!({
             "foo": "bar",
             "hello": 52
         }));
@@ -132,19 +167,19 @@ mod tests {
 
     #[test]
     fn decode_empty_bencoded_dictionary() {
-        assert_eq!(decode_bencoded_value("de"), json!({}));
+        assert_eq!(decode_bencoded_from_str("de").as_json(), json!({}));
     }
 
     #[test]
     fn decode_bencoded_dictionary_which_misses_value() {
-        assert_eq!(decode_bencoded_value("d3:foo3:bar5:helloe"), json!({
+        assert_eq!(decode_bencoded_from_str("d3:foo3:bar5:helloe").as_json(), json!({
             "foo": "bar"
         }));
     }
 
     #[test]
     fn decode_bencoded_nested_object() {
-        assert_eq!(decode_bencoded_value("d10:inner_dictd4:key16:value14:key2i42eee"), json!({
+        assert_eq!(decode_bencoded_from_str("d10:inner_dictd4:key16:value14:key2i42eee").as_json(), json!({
             "inner_dict": {
                 "key1":"value1",
                 "key2":42
@@ -154,7 +189,7 @@ mod tests {
 
     #[test]
     fn decode_bencoded_objects_in_an_array() {
-        assert_eq!(decode_bencoded_value("ld4:key16:value1ed4:key26:value2ed4:key36:value3ee"), json!([
+        assert_eq!(decode_bencoded_from_str("ld4:key16:value1ed4:key26:value2ed4:key36:value3ee").as_json(), json!([
             {
                 "key1": "value1"
             },
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::env;
 
 mod bencoded;
 
+struct Torrent {
+
+}
+
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,8 +16,10 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = bencoded::decode_bencoded_value(encoded_value);
+        let decoded_value = bencoded::decode_bencoded_from_str(encoded_value).as_json();
         println!("{}", decoded_value.to_string());
+    } else if command == "info" {
+        let torrent_file = &args[2];
     } else {
         println!("unknown command: {}", args[1])
     }
